14b192ecacb77619ad5a3791e0d1790cb596baaa
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-01 14:27:28 +0100

    Create 102-zombie.c

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - function that contains an infinite loop
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int i;
+    pid_t child_pid;
+
+    for (i = 0; i < 5; i++)
+    {
+        child_pid = fork();
+        if (child_pid == 0)
+        {
+            dprintf(1, "Zombie process created, PID: %d\n", getpid());
+            return (0);
+        }
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        wait(NULL);
+    }
+
+    infinite_while();
+    return (0);
+}

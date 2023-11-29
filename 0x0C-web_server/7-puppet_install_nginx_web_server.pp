56ceb903970ee31db28631c79267025647e4d042
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-11-29 04:00:12 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 $default_site_loc = '/etc/nginx/sites-available/default'
-$site_available = 'https://raw.githubusercontent.com/Shindou7/alx-system_engineering-devops/master/0x0C-web_server/site_available'
+$default_site = 'https://raw.githubusercontent.com/Shindou7/alx-system_engineering-devops/master/0x0C-web_server/site_available'
 
 # Run apt-get update
 exec { 'apt-update':
@@ -26,35 +26,15 @@ file { 'Create 404.html':
   content => 'Ceci n\'est pas une page\n',
 }
 
-# Replace site_available config
-exec { 'Replace config':
-  command => "/usr/bin/curl ${site_available} > ${default_site_loc}",
-  require => File["/etc/nginx/sites-available/default"],
-}
-
-# Nginx site config
+# Replace default site config
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => template('path/to/your/template.erb'),
-  require => Exec['Replace config'],
-}
-
-# Create a symbolic link to sites-enabled
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  require => File["/etc/nginx/sites-available/default"],
+  source  => $default_site,
+  require => Package['nginx'],
 }
 
 # Start nginx service
 service { 'nginx':
   ensure  => running,
-  require => Exec['Replace config'],
-}
-
-# Configure the redirect
-nginx::resource::location { 'redirect_me':
-  ensure       => present,
-  location     => '/redirect_me',
-  rewrite_rule => '^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent',
+  require => File["/etc/nginx/sites-available/default"],
 }

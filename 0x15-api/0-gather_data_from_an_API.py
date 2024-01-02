f8c5fae36746cbf0a2db91e7c659350a84ec5963
Shindou <98887570+Shindou7@users.noreply.github.com>
2024-01-02 23:59:01 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
 '''
-Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+Write a Python script that, using this REST API
 '''
 import requests
 from sys import argv
 
-def display(employee_id):
-    # Fetch user data
-    users = requests.get("https://jsonplaceholder.typicode.com/users")
-    user_data = next((user for user in users.json() if user.get('id') == employee_id), None)
 
-    if user_data is None:
-        print(f"Error: Employee with ID {employee_id} not found.")
-        return
-
-    EMPLOYEE_NAME = user_data.get('name')
-
-    # Fetch TODO data
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
-    user_todos = [todo for todo in todos.json() if todo.get('userId') == employee_id]
-
-    TOTAL_NUM_OF_TASKS = len(user_todos)
-    NUMBER_OF_DONE_TASKS = sum(1 for todo in user_todos if todo.get('completed'))
-    TASK_TITLE = [todo.get('title') for todo in user_todos if todo.get('completed')]
-
+def display():
+    """return API data"""
+    users = requests.get("http://jsonplaceholder.typicode.com/users")
+    for user in users.json():
+        if user.get('id') == int(argv[1]):
+            EMPLOYEE_NAME = (user.get('name'))
+            break
+    TOTAL_NUM_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for todo in todos.json():
+        if todo.get('userId') == int(argv[1]):
+            TOTAL_NUM_OF_TASKS += 1
+            if todo.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(todo.get('title'))
     print("Employee {} is done with tasks({}/{}):"
           .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUM_OF_TASKS))
-    
     for task in TASK_TITLE:
         print("\t {}".format(task))
 
+
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python 0-gather_data_from_an_API.py <Employee_ID>")
-        exit()
-
-    try:
-        employee_id = int(argv[1])
-    except ValueError:
-        print("Error: Invalid Employee ID")
-        exit()
-
-    display(employee_id)
+    display()

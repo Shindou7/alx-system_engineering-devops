07573fd57777bf8bb821a0a9d50e18ad779e7e19
Shindou7 <sh.shindou@gmail.com>
2024-01-02 14:30:54 -0800

    j

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,6 +5,7 @@ gather employee data from API
 import requests
 from sys import argv
 
+
 def display():
     """return API data"""
     users = requests.get("http://jsonplaceholder.typicode.com/users")
@@ -12,11 +13,11 @@ def display():
         if user.get('id') == int(argv[1]):
             EMPLOYEE_NAME = user.get('name')
             break
-    
+
     TOTAL_NUM_OF_TASKS = 0
     NUMBER_OF_DONE_TASKS = 0
     TASK_TITLE = []
-    
+
     todos = requests.get("http://jsonplaceholder.typicode.com/todos")
     for todo in todos.json():
         if todo.get('userId') == int(argv[1]):
@@ -24,14 +25,15 @@ def display():
             if todo.get('completed') is True:
                 NUMBER_OF_DONE_TASKS += 1
                 TASK_TITLE.append(todo.get('title'))
-    
+
     # Corrected formatting for the first line
     print("Employee {} is done with tasks({}/{}):"
           .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUM_OF_TASKS))
-    
+
     # Corrected formatting for the completed tasks
     for task in TASK_TITLE:
         print("\t{}".format(task))
+
 
 if __name__ == "__main__":
     display()

251fd682be69a08d049941a01a3ddae94a739ec8
Shindou <98887570+Shindou7@users.noreply.github.com>
2024-01-02 23:47:03 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,55 +1,43 @@
 #!/usr/bin/python3
-'''
-Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-'''
+"""Given an Employee ID, returns information
+about his/her TODO list progress.
+"""
 import requests
 from sys import argv
 
+if __name__ == '__main__':
+    try:
+        emp_id = int(argv[1])
+    except ValueError:
+        exit()
 
-def gather_data(employee_id):
-    """Retrieve data from the API for a given employee ID"""
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    api_url = 'https://jsonplaceholder.typicode.com'
+    user_uri = '{api}/users/{id}'.format(api=api_url, id=emp_id)
+    todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
 
-    # Fetch user data
-    users_response = requests.get(users_url)
-    users = users_response.json()
+    # User Response
+    res = requests.get(user_uri).json()
 
-    employee_name = None
+    # Name of the employee
+    name = res.get('name')
 
-    for user in users:
-        if user.get('id') == employee_id:
-            employee_name = user.get('name')
-            break
+    # User TODO Response
+    res = requests.get(todo_uri).json()
 
-    if employee_name is None:
-        print("Employee with ID {} not found.".format(employee_id))
-        return
+    # Total number of tasks, the sum of completed and non-completed tasks
+    total = len(res)
 
-    # Fetch TODOs for the given employee ID
-    todos_response = requests.get(todos_url, params={'userId': employee_id})
-    todos = todos_response.json()
+    # Number of non-completed tasks
+    non_completed = sum([elem['completed'] is False for elem in res])
 
-    # Process TODOs
-    total_tasks = len(todos)
-    completed_tasks = [task for task in todos if task.get('completed')]
-    num_completed_tasks = len(completed_tasks)
+    # Number of completed tasks
+    completed = total - non_completed
 
-    # Display information
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, num_completed_tasks, total_tasks))
+    # Formatting the expected output
+    str = "Employee {emp_name} is done with tasks({completed}/{total}):"
+    print(str.format(emp_name=name, completed=completed, total=total))
 
-    for task in completed_tasks:
-        print("\t{}".format(task.get('title')))
-
-
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: {} <employee_id>".format(argv[0]))
-    else:
-        try:
-            employee_id = int(argv[1])
-            gather_data(employee_id)
-        except ValueError:
-            print("Invalid employee ID. Please provide a valid integer.")
+    # Printing completed tasks
+    for elem in res:
+        if elem.get('completed') is True:
+            print('\t', elem.get('title'))

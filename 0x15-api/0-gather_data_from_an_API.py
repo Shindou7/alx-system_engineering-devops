b39219f5e3383b8c6e650aa436e18caf91c7560e
Shindou <98887570+Shindou7@users.noreply.github.com>
2024-01-02 22:41:39 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,29 +5,38 @@ about his/her TODO list progress.
 import requests
 from sys import argv
 
-
 def display():
-    """Script that uses JSONPlaceholder API"""
-    users = requests.get("http://jsonplaceholder.typicode.com/users")
-    for user in users.json():
-        if user.get('id') == int(argv[1]):
-            EMPLOYEE_NAME = (user.get('name'))
-            break
-    TOTAL_NUM_OF_TASKS = 0
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = []
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for todo in todos.json():
-        if todo.get('userId') == int(argv[1]):
-            TOTAL_NUM_OF_TASKS += 1
-            if todo.get('completed') is True:
-                NUMBER_OF_DONE_TASKS += 1
-                TASK_TITLE.append(todo.get('title'))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUM_OF_TASKS))
-    for task in TASK_TITLE:
-        print("\t {}".format(task))
+    if len(argv) != 2:
+        print("Usage: {} <employee_id>".format(argv[0]))
+        return
 
+    employee_id = int(argv[1])
+
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    for user in users.json():
+        if user.get('id') == employee_id:
+            employee_name = user.get('name')
+            break
+    else:
+        print("Employee with ID {} not found.".format(employee_id))
+        return
+
+    total_num_of_tasks = 0
+    number_of_done_tasks = 0
+    task_titles = []
+
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    for todo in todos.json():
+        if todo.get('userId') == employee_id:
+            total_num_of_tasks += 1
+            if todo.get('completed'):
+                number_of_done_tasks += 1
+                task_titles.append(todo.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, number_of_done_tasks, total_num_of_tasks))
+    for task in task_titles:
+        print("\t{}".format(task))
 
 if __name__ == "__main__":
     display()
